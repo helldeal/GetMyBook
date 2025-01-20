9eb94cea2aaa54024cf09bd73b7c43a9e387321e
Alexandre Clenet <alexandreclenet@free.fr>
2025-01-20 10:48:02 +0100

    working app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
 import { View, StatusBar } from "react-native";
 import Navigation from "./Navigation";
-import { IcsProvider } from "./hooks/icsProvider";
-import { EventsProvider } from "./hooks/eventsProvider";
-import { CalendarProvider } from "./hooks/calendarProvider";
 
 export default function App() {
   return (
-    <IcsProvider>
-      <CalendarProvider>
-        <EventsProvider>
-          <View style={{ flex: 1 }} className="mt-0">
-            <Navigation />
-            <StatusBar
-              barStyle={"dark-content"}
-              backgroundColor="transparent"
-            />
-          </View>
-        </EventsProvider>
-      </CalendarProvider>
-    </IcsProvider>
+    <View style={{ flex: 1 }} className="mt-0">
+      <Navigation />
+      <StatusBar barStyle={"dark-content"} backgroundColor="transparent" />
+    </View>
   );
 }
