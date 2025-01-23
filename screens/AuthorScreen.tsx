2fd18b26083f3974feb7025274f98413f16c8ee4
Melvin SIMON <msimon@la-joliverie.com>
2025-01-23 11:12:29 +0100

    feat: author page

--- screens/AuthorScreen.tsx
+++ screens/AuthorScreen.tsx
@@ -1,36 +1,63 @@
-import { useIsFocused } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { Text, SafeAreaView, ScrollView } from "react-native";
-import { getAuthor } from "../api/books";
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+import { getAuthor, getEditionFromAuthor } from "../api/books";
 import Loading from "../components/Loading";
 
 export const AuthorScreen = ({ route, navigation }: any) => {
   const { authorKey } = route.params;
   const [author, setAuthor] = useState<any>({});
+  const [books, setBooks] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const isFocused = useIsFocused();
 
   const init = async () => {
-    const author = await getAuthor(authorKey);
-    console.log("Author: ", author.name);
-    setAuthor(author);
-    setLoading(false);
+    try {
+      const authorData = await getAuthor(authorKey);
+      setAuthor(authorData);
+
+      const booksData = await getEditionFromAuthor(authorKey);
+      setBooks(booksData.entries || []); // Assurez-vous que `entries` est défini
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données :", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     if (isFocused) {
-      console.log("Nav on Author Page : ", authorKey);
       init();
     }
   }, [isFocused]);
+
+  const renderBook = ({ item }: { item: any }) => (
+    <TouchableOpacity
+      className="bg-white border-b-[#e5e5e5] flex justify-start align-middle dark:bg-[#131f24] dark:border-b-[#37464f]"
+      onPress={() => navigation.navigate("BookScreen", { worksKey: item.key })}
+    >
+      <View className="ml-4 py-3 flex-1 flex-row justify-start items-center">
+        <Text className="font-bold text-lg">{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <SafeAreaView className=" flex justify-start bg-white w-full h-full">
+    <SafeAreaView className="flex justify-start bg-white w-full h-full">
       {loading ? (
         <Loading />
       ) : (
-        <ScrollView className="gap-2" showsVerticalScrollIndicator={false}>
-          <Text>{author.name}</Text>
-        </ScrollView>
+        <FlatList
+          data={books}
+          keyExtractor={(item) => item.key} // Utilise `key` comme identifiant unique
+          renderItem={renderBook} // Rend chaque élément
+        />
       )}
     </SafeAreaView>
   );
